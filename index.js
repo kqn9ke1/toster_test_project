409056a9d493441043ced1417d75e7c8e8a8f3ac
kqn9ke1 <kanykei.daiyrbekovna@gmail.com>
2023-09-24 11:49:03 +0600

    add style

---
+++ index.js
@@ -0,0 +1,60 @@
+// timer
+document.addEventListener("DOMContentLoaded", () => {
+  const end = new Date("Sep 24 2023 05:00:00");
+
+  const hourVal = document.querySelector(".time .hour");
+  const minuteVal = document.querySelector(".time .minutes");
+  const secondVal = document.querySelector(".time .second");
+
+  const timeCount = () => {
+    let now = new Date();
+    let leftUntil = end - now;
+
+    let hours = Math.floor(leftUntil / 1000 / 60 / 60) % 24;
+    let minutes = Math.floor(leftUntil / 1000 / 60) % 60;
+    let seconds = Math.floor(leftUntil / 1000) % 60;
+
+    hourVal.innerHTML = hours < 9 ? "0" + hours + ":" : hours + ":";
+    minuteVal.innerHTML = minutes < 9 ? "0" + minutes + ":" : minutes + ":";
+    secondVal.innerHTML = seconds < 9 ? "0" + seconds : seconds;
+  };
+  timeCount();
+  setInterval(timeCount, 1000);
+});
+
+// image of carousels
+function carousel() {}
+//
+function myFunction() {
+  document.querySelector("#myDropdown").classList.toggle("show");
+}
+// Закрыть раскрывающийся список, если пользователь щелкнет за его пределами.
+window.onclick = function (event) {
+  if (!event.target.matches(".sixth_column_select")) {
+    var dropdowns = document.querySelector("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+
+// button
+const button = document.querySelector(".button_buy");
+button.addEventListener("click", () => {
+  window.location.href = "https://youtube.com";
+});
+
+//
+document.body.addEventListener("click", (e) => {
+  if (!e.target.matches("button")) return;
+  document.querySelector(".pic img").src = e.target.dataset.src;
+
+  document
+    .querySelectorAll("button")
+    .forEach((btn) => btn.classList.remove("active"));
+  e.target.classList.add("active");
+});
